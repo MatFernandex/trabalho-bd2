7be11668be2123d4c0a115f9c3aa54e2d134fd8d
Pedro Bueno <pedrolbb.e@gmail.com>
2024-06-21 14:25:47 -0300

    chore: update default server PORT

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -6,7 +6,7 @@ dotenv.config()
 export const AppConfig = registerAs(
   'app',
   (): Record<string, any> => ({
-    API_PORT: Number(process.env.API_PORT ?? 3000),
+    API_PORT: Number(process.env.API_PORT ?? 4000),
     API_GLOBAL_PREFIX: process.env.API_GLOBAL_PREFIX ?? 'api',
     DATABASE_URL: String(process.env.DATABASE_URL),
   }),
