a24582c7761bce676c44ec9ab25785372b3a6d7d
Pedro Bueno <pedrolbb.e@gmail.com>
2024-06-21 15:46:36 -0300

    chore: add jwt envs

--- env.d.ts
+++ env.d.ts
@@ -4,6 +4,9 @@ declare global {
       API_PORT?: number
       API_GLOBAL_PREFIX?: string
 
+      JWT_SECRET_KEY: string
+      JWT_EXPIRATION_TIME: string
+
       DATABASE_URL: string
     }
   }
--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -8,6 +8,8 @@ export const AppConfig = registerAs(
   (): Record<string, any> => ({
     API_PORT: Number(process.env.API_PORT ?? 4000),
     API_GLOBAL_PREFIX: process.env.API_GLOBAL_PREFIX ?? 'api',
+    JWT_SECRET_KEY: String(process.env.JWT_SECRET_KEY),
+    JWT_EXPIRATION_TIME: String(process.env.JWT_EXPIRATION_TIME),
     DATABASE_URL: String(process.env.DATABASE_URL),
   }),
 )
