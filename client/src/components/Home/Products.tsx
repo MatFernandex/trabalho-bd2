f0f5e12ce1f0ba8a1772ef1f13b07a0ddb6236f3
Pedro Bueno <pedrolbb.e@gmail.com>
2024-06-25 17:23:01 -0300

    feat(client): add logged as in Products panel

--- client/src/components/Home/Products.tsx
+++ client/src/components/Home/Products.tsx
@@ -101,76 +101,83 @@ const Products = () => {
     [user?.fun_codigo, getProducts, token],
   )
 
+  const isUser = Boolean(user?.usu_codigo)
+
   useEffect(() => {
     void getProducts()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-12">
-      {products.map((product) => (
-        <Card key={product.pro_codigo} className="overflow-hidden shadow-lg">
-          {editingProduct && editingProduct.pro_codigo === product.pro_codigo ? (
-            <>
-              <CardHeader>
-                <CardTitle>Código do Produto: {product.pro_codigo}</CardTitle>
-                <input
-                  defaultValue={product.pro_descricao}
-                  onChange={(e) => (product.pro_descricao = e.target.value)}
-                />
-              </CardHeader>
-              <CardContent>
-                <input
-                  defaultValue={product.pro_valor}
-                  onChange={(e) => (product.pro_valor = parseFloat(e.target.value))}
-                />
-                <input
-                  defaultValue={product.pro_quantidade}
-                  onChange={(e) => (product.pro_quantidade = parseInt(e.target.value))}
-                />
-                <p className="text-gray-700 dark:text-gray-400 text-base">Fornecedor: {product.tb_fornecedor}</p>
-              </CardContent>
-              <CardFooter className="flex flex-row gap-x-2">
-                <Button onClick={async () => await handleSave(product.pro_codigo, product)}>Salvar</Button>
-                <Button variant="destructive" onClick={() => setEditingProduct(null)}>
-                  Cancelar
-                </Button>
-              </CardFooter>
-            </>
-          ) : (
-            <>
-              <CardHeader>
-                <CardTitle>Código do Produto: {product.pro_codigo}</CardTitle>
-                <CardDescription>Descrição: {product.pro_descricao}</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p className="text-gray-700 dark:text-gray-400 text-base">Valor: {product.pro_valor}</p>
-                <p className="text-gray-700 dark:text-gray-400 text-base">Quantidade: {product.pro_quantidade}</p>
-                <p className="text-gray-700 dark:text-gray-400 text-base">Fornecedor: {product.tb_fornecedor}</p>
-              </CardContent>
-              <CardFooter className="flex flex-col gap-y-2">
-                {user?.fun_codigo && (
+    <div>
+      <div className="px-12">
+        <p className="text-lg uppercase">Painel de {isUser ? 'usuário' : 'funcionário'}.</p>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-12">
+        {products.map((product) => (
+          <Card key={product.pro_codigo} className="overflow-hidden shadow-lg">
+            {editingProduct && editingProduct.pro_codigo === product.pro_codigo ? (
+              <>
+                <CardHeader>
+                  <CardTitle>Código do Produto: {product.pro_codigo}</CardTitle>
                   <input
-                    type="number"
-                    min="1"
-                    max={product.pro_quantidade}
-                    defaultValue="1"
-                    onChange={(e) => setQuantity(parseInt(e.target.value))}
+                    defaultValue={product.pro_descricao}
+                    onChange={(e) => (product.pro_descricao = e.target.value)}
                   />
-                )}
-                <div className="flex flex-row gap-x-2">
-                  {user?.fun_codigo && (
-                    <Button onClick={async () => await handleSell(product.pro_codigo, quantity)}>Vender</Button>
-                  )}
-                  <Button variant="secondary" onClick={() => handleEdit(product)}>
-                    Editar
+                </CardHeader>
+                <CardContent>
+                  <input
+                    defaultValue={product.pro_valor}
+                    onChange={(e) => (product.pro_valor = parseFloat(e.target.value))}
+                  />
+                  <input
+                    defaultValue={product.pro_quantidade}
+                    onChange={(e) => (product.pro_quantidade = parseInt(e.target.value))}
+                  />
+                  <p className="text-gray-700 dark:text-gray-400 text-base">Fornecedor: {product.tb_fornecedor}</p>
+                </CardContent>
+                <CardFooter className="flex flex-row gap-x-2">
+                  <Button onClick={async () => await handleSave(product.pro_codigo, product)}>Salvar</Button>
+                  <Button variant="destructive" onClick={() => setEditingProduct(null)}>
+                    Cancelar
                   </Button>
-                </div>
-              </CardFooter>
-            </>
-          )}
-        </Card>
-      ))}
+                </CardFooter>
+              </>
+            ) : (
+              <>
+                <CardHeader>
+                  <CardTitle>Código do Produto: {product.pro_codigo}</CardTitle>
+                  <CardDescription>Descrição: {product.pro_descricao}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-gray-700 dark:text-gray-400 text-base">Valor: {product.pro_valor}</p>
+                  <p className="text-gray-700 dark:text-gray-400 text-base">Quantidade: {product.pro_quantidade}</p>
+                  <p className="text-gray-700 dark:text-gray-400 text-base">Fornecedor: {product.tb_fornecedor}</p>
+                </CardContent>
+                <CardFooter className="flex flex-col gap-y-2">
+                  {user?.fun_codigo && (
+                    <input
+                      type="number"
+                      min="1"
+                      max={product.pro_quantidade}
+                      defaultValue="1"
+                      onChange={(e) => setQuantity(parseInt(e.target.value))}
+                    />
+                  )}
+                  <div className="flex flex-row gap-x-2">
+                    {user?.fun_codigo && (
+                      <Button onClick={async () => await handleSell(product.pro_codigo, quantity)}>Vender</Button>
+                    )}
+                    <Button variant="secondary" onClick={() => handleEdit(product)}>
+                      Editar
+                    </Button>
+                  </div>
+                </CardFooter>
+              </>
+            )}
+          </Card>
+        ))}
+      </div>
     </div>
   )
 }
